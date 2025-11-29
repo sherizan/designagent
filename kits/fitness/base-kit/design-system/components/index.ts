09dacff7a7386eb118f1bb5fa19e40d4c89594c1
Sherizan <sherizan@gmail.com>
2025-11-29 09:51:47 +0400

    Remove UI Kits feature (Fitness Kit) and clean up project
    - Remove all Fitness UI Kit components and related code
    - Remove UI Kit gallery and card components
    - Remove kits route and API endpoint for downloading kits
    - Remove kits folder and all fitness-related assets
    - Clean up unused kit-copy-button component
    - Project now focuses solely on Screens and Components features

--- kits/fitness/base-kit/design-system/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { Card } from "./Card";
-export { StatCard } from "./StatCard";
-export { WorkoutListItem } from "./WorkoutListItem";
-export { LoadingShimmer } from "./LoadingShimmer";
-
--- kits/fitness/base-kit/design-system/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./theme";
-export * from "./FontsProvider";
-export * from "./components";
-
